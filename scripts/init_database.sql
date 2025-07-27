69e92e6596be2f2bdb79e97953db6d03ebdb1bfa
Prem Jamdade <158582007+prem-jamdade@users.noreply.github.com>
2025-07-27 20:48:01 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,20 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'. 
+    If the database already exists, it will be dropped and recreated.
+
+    The script also creates the following schemas within the database:
+      - bronze  : for raw ingested data from CRM and ERP sources
+      - silver  : for cleaned and integrated data
+      - gold    : for the final star schema (fact and dimension tables)
+
+WARNING:
+    Running this script will DROP the existing 'DataWarehouse' database 
+    if it exists. All existing data will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
 
